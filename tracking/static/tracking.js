61e1fac11b6c66c384251fd79f75a9322a49ebda
John Zolton <jgz6@uakron.edu>
2023-01-08 17:01:28 -0500

    added search bar

--- tracking/static/tracking.js
+++ tracking/static/tracking.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('success')
+    document.querySelector('#submit').addEventListener('click', lookupfood);
 })
+
+function lookupfood() {
+    console.log('form clicked')
+    let food = document.querySelector('#food').value;
+    console.log(food)
+    return false;
+}
--- tracking/views.py
+++ tracking/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'GET':
+        return render(request, 'index.html')
+        
+    if request.method == 'POST':
+        print(request.POST['food'])
+        return render(request, 'index.html')
