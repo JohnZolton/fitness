44f42072b02553320c8a967fa1f0f0eb6f026de6
John Zolton <jgz6@uakron.edu>
2023-01-09 13:59:13 -0500

    enabled querying usda api

--- tracking/static/tracking.js
+++ tracking/static/tracking.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#submit').addEventListener('click', lookupfood);
+    document.querySelector('form').onsubmit = search();
 })
 
-function lookupfood() {
-    console.log('form clicked')
-    let food = document.querySelector('#food').value;
-    console.log(food)
-    return false;
+function search() {
+    console.log('yes')
+    let api = ''
+    let url = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key='
+    let suffix = '&query='
+    let food = document.querySelector('#food').value
+    fetch(url + api + suffix + food)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
--- tracking/views.py
+++ tracking/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
+import requests
+import json
+import time
 
-# Create your views here.
-
+api_key = ''
 def index(request):
     if request.method == 'GET':
         return render(request, 'index.html')
         
     if request.method == 'POST':
-        print(request.POST['food'])
+        food = request.POST['food']
+        url = f'https://api.nal.usda.gov/fdc/v1/foods/search?api_key={api_key}&query={food}'
+
+        data = requests.get(url).json()
+        
+        nutrients = {'protein': 0, 'carbs':2, 'fats': 1, 'fiber':9, 'calories':3}
+
+        for item in data['foods']:
+            print(item['description'])
+            for i in nutrients.values():
+                try:
+                    print(item['foodNutrients'][i]['nutrientName'], item['foodNutrients'][i]['value'],item['foodNutrients'][i]['unitName'])
+                except IndexError:
+                    print(':)')
         return render(request, 'index.html')
