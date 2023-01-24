653743e2edf617ce2a38434395e0089e70e15573
John Zolton <jgz6@uakron.edu>
2023-01-23 19:24:05 -0500

    enabled dark/light mode

--- tracking/static/tracking.js
+++ tracking/static/tracking.js
@@ -697,3 +697,25 @@ function getcookie(name) {
     }
     return cookievalue;
 }
+
+function changetheme(){
+    if (localStorage.getItem('theme') === 'darkmode') {
+        settheme('lightmode');
+    } else {
+        settheme('darkmode');
+    }
+}
+
+function settheme(theme){
+    localStorage.setItem('theme', theme)
+    document.documentElement.className= theme
+}
+
+
+(function (){
+    if (localStorage.getItem('theme') === 'darkmode') {
+        settheme('darkmode');
+    } else {
+        settheme('lightmode');
+    }
+})()
